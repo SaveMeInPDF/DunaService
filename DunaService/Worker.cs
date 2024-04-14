5097fc9125b209e8d513ea686c35e6e0f58a9064
ivang <rare.nuun@gmail.com>
2024-04-14 20:42:25 +0300

    1st commit

--- DunaService/Worker.cs
+++ DunaService/Worker.cs
@@ -1,19 +1,58 @@
-namespace DunaService;
+using System.Security.Cryptography;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
+namespace DunaService;
+
+
 public class Worker : BackgroundService
 {
-    private readonly string hostname = "localhost";
+    private readonly string _hostname = "localhost";
     private readonly ILogger<Worker> _logger;
+    private IConnection connection;
+    private IModel channel;
+    private EventingBasicConsumer consumer;
+    private void Received(object? model, BasicDeliverEventArgs? ea)
+    {
+        if (ea == null) return;
+        var body = ea.Body.ToArray();
+        // 4 байта - ip, 8 байт - вес файла в байтах, остальное - имя файла и сам файл
+        var ip = body[..4];
+        var weight = BitConverter.ToInt32(body[4..8]);
+        var name = Encoding.UTF8.GetString(body[8..^weight]);
+        var file = body[^weight..^1];
+    }
+    
+    // пробросить всё содержимое сообщения через хэш-функцию, таким образом сгенерировать новое имя файла (токен) в 64 символа
+    // сохранить файл в папку /files
+    // добавить запись в базу данных такого содержания:
+    // ip, название файла, токен, размер, UNIX-время, счётчик до удаления (24)
+    
+    private string Hash(byte[] data)
+    {
+        using var sha = SHA256.Create();
+        var hash = sha.ComputeHash(data);
+        return BitConverter.ToString(hash).Replace("-", "").ToLower();
+    }
 
     public Worker(ILogger<Worker> logger)
     {
-        var factory = new ConnectionFactory { HostName = hostname };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+        ConnectionFactory factory = new ConnectionFactory { HostName = _hostname };
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
         _logger = logger;
+        
+        channel.QueueDeclare(queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        consumer = new EventingBasicConsumer(channel);
+        consumer.Received += Received;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,6 +63,10 @@ public class Worker : BackgroundService
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+            
+            channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
 
             await Task.Delay(1000, stoppingToken);
         }
