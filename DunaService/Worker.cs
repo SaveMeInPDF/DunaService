bd3206de05addca5c76b56ec565d4a36e6caf520
Alex Trifonov <ta.alexashow@ya.ru>
2024-04-14 20:49:49 +0300

    Переделал в RPC

--- DunaService/Worker.cs
+++ DunaService/Worker.cs
@@ -16,18 +16,35 @@ public class Worker : BackgroundService
     private void Received(object? model, BasicDeliverEventArgs? ea)
     {
         if (ea == null) return;
+        
+        string response = string.Empty;
+        var props = ea.BasicProperties;
+        var replyProps = channel.CreateBasicProperties();
+        replyProps.CorrelationId = props.CorrelationId;
+        
+        // тут ты работаеш, вызываеш функции, обрабатываешь эту хрень
+        
         var body = ea.Body.ToArray();
         // 4 байта - ip, 8 байт - вес файла в байтах, остальное - имя файла и сам файл
         var ip = body[..4];
         var weight = BitConverter.ToInt32(body[4..8]);
         var name = Encoding.UTF8.GetString(body[8..^weight]);
         var file = body[^weight..^1];
+        
+        // пробросить всё содержимое сообщения через хэш-функцию, таким образом сгенерировать новое имя файла (токен) в 63 символа
+        // сохранить файл в папку /files
+        // добавить запись в базу данных такого содержания:
+        // ip, название файла, токен, размер, UNIX-время, счётчик до удаления (23)
+        
+        // после того, как ты её обработал, тебе нужно в response записать токен строкой
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(exchange: string.Empty,
+            routingKey: props.ReplyTo,
+            basicProperties: replyProps,
+            body: responseBytes);
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
     }
     
-    // пробросить всё содержимое сообщения через хэш-функцию, таким образом сгенерировать новое имя файла (токен) в 64 символа
-    // сохранить файл в папку /files
-    // добавить запись в базу данных такого содержания:
-    // ip, название файла, токен, размер, UNIX-время, счётчик до удаления (24)
     
     private string Hash(byte[] data)
     {
@@ -43,13 +60,13 @@ public class Worker : BackgroundService
         channel = connection.CreateModel();
         _logger = logger;
         
-        channel.QueueDeclare(queue: "hello",
+        channel.QueueDeclare(queue: "rpc_queue",
             durable: false,
             exclusive: false,
             autoDelete: false,
             arguments: null);
-
-        Console.WriteLine(" [*] Waiting for messages.");
+        
+        channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
         consumer = new EventingBasicConsumer(channel);
         consumer.Received += Received;
